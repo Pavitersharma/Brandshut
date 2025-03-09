f40f004162b94289bdad0ffdb03fa09d79043799
Pavitersharma <paviterattri28@gmail.com>
2025-03-09 23:12:15 +0530

    changes

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -8,6 +8,39 @@ export default function Dashboard() {
             <p>
             Manage your tasks, track progress, and stay organized—all in one place. Let’s get started!"
             </p>
+            <div className="">
+              <div className="row">
+                <div className="col-md-4 ">
+                    <div className="p-4 shadow ">
+                    <h5>Recent Order</h5>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, atque. Unde voluptas atque similique iure modi nisi provident aliquid maxime quam! Fuga velit at maiores dolores, magni suscipit aperiam et.</p>
+                    <a href="" className='text-decoration-underline'>View more</a>
+                    </div>
+                </div>
+                <div className="col-md-4">
+                  <div className="p-4 shadow">
+                    <h5>Transactions</h5>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, consectetur. Tenetur quisquam sit, nihil enim aspernatur blanditiis, omnis ea culpa laudantium, unde quidem! Possimus vero ipsum dolorem! Vero, necessitatibus voluptate.</p>
+                    <a href="" className='text-decoration-underline'>view Transactions</a>
+                  </div>
+                </div>
+                <div className="col-md-4">
+                  <div className="p-4 shadow">
+                    <h5>Adressess</h5>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, consectetur. Tenetur quisquam sit, nihil enim aspernatur blanditiis, omnis ea culpa laudantium, unde quidem! Possimus vero ipsum dolorem! Vero, necessitatibus voluptate.</p>
+                    <a href="" className='text-decoration-underline'>view Adressess</a>
+                  </div>
+                </div>
+                <div className="col-md-4">
+                  <div className="p-4 shadow">
+                    <h5>My Account</h5>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, consectetur. Tenetur quisquam sit, nihil enim aspernatur blanditiis, omnis ea culpa laudantium, unde quidem! Possimus vero ipsum dolorem! Vero, necessitatibus voluptate.</p>
+                    <a href="" className='text-decoration-underline'>Edit Pro
+                      file</a>
+                  </div>
+                </div>
+              </div>
+            </div>
 
             
 
